b443c9fc684080b5afa12fefe67ca37ecbcdcc5d
João Batista <joao42lbatista@gmail.com>
2016-09-22 14:27:36 -0300

    Add cool site for coding

---
+++ cool_sites_coding.r
@@ -0,0 +1,21 @@
+## Cool sites for coding
+
+## Hackerrank
+https://www.hackerrank.com/
+
+## URI Online Judge - Problems & Contests
+https://www.urionlinejudge.com.br/
+
+## Interview Bit
+https://www.interviewbit.com/
+
+## Code Fights
+https://codefights.com/
+
+## Live Coding
+https://www.livecoding.tv/
+
+## Fun Sites
+https://www.nayuki.io/
+https://nihey.org/
+https://try.github.io/
