006d0ff85ee3eb48e0e331bb8a1060cade064c4c
João Batista <joao42lbatista@gmail.com>
2016-11-20 10:07:44 -0200

    Add link-generate_git-clone_JBs.sh

---
+++ link-generate_git-clone_JBs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gitCloneFOlder="/media/sda4/prog/git_clone"
+dropboxFolder="/media/sda4/prog/Dropbox"
+
+ln -s $gitCloneFOlder/1_0txt/ $dropboxFolder/0_pessoal/0txt
+
+ln -s $gitCloneFOlder/3_dissertation_tex/ $dropboxFolder/mestrado/dissertacao/dissertation_tex
+
+ln -s $gitCloneFOlder/4_paper_01_mestrado/ $dropboxFolder/mestrado/dissertacao/paper_01_mestrado
+
+ln -s $gitCloneFOlder/7_SUMO_OMNeT_Veins_Slackware $dropboxFolder/mestrado/SUMO_OMNeT_Veins_Slackware
+
+ln -s $gitCloneFOlder/8_veins_simulation/ $dropboxFolder/mestrado/dissertacao/veins_simulation
