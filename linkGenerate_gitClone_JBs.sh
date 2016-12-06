69104e214ea4d1a6a801a5e3c30364a599231a99
João Batista <joao42lbatista@gmail.com>
2016-12-06 09:35:27 -0200

    # Mv 0txt => cool_dark_things Up ln -s to full name

--- linkGenerate_gitClone_JBs.sh
+++ linkGenerate_gitClone_JBs.sh
@@ -3,12 +3,12 @@
 gitCloneFOlder="/media/sda4/prog/git_clone"
 dropboxFolder="/media/sda4/prog/Dropbox"
 
-ln -s $gitCloneFOlder/1_0txt/ $dropboxFolder/0_pessoal/0txt
+ln -s $gitCloneFOlder/1_cool_dark_things/ $dropboxFolder/0_pessoal/
 
-ln -s $gitCloneFOlder/3_dissertation_tex/ $dropboxFolder/mestrado/dissertacao/dissertation_tex
+ln -s $gitCloneFOlder/3_dissertation_tex/ $dropboxFolder/mestrado/dissertacao/
 
-ln -s $gitCloneFOlder/4_paper_01_mestrado/ $dropboxFolder/mestrado/dissertacao/paper_01_mestrado
+ln -s $gitCloneFOlder/4_paper_01_mestrado/ $dropboxFolder/mestrado/dissertacao/
 
-ln -s $gitCloneFOlder/7_SUMO_OMNeT_Veins_Slackware $dropboxFolder/mestrado/SUMO_OMNeT_Veins_Slackware
+ln -s $gitCloneFOlder/7_SUMO_OMNeT_Veins_Slackware $dropboxFolder/mestrado/
 
-ln -s $gitCloneFOlder/8_veins_simulation/ $dropboxFolder/mestrado/dissertacao/veins_simulation
+ln -s $gitCloneFOlder/8_veins_simulation/ $dropboxFolder/mestrado/dissertacao/
