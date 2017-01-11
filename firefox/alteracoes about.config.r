fd78ed8e426f72f5a11dc6f86b7b0c1d6bae7ed9
João Batista <joao42lbatista@gmail.com>
2017-01-11 12:33:05 -0200

    Up alteracoes about.config.r

--- firefox/alteracoes about.config.r
+++ firefox/alteracoes about.config.r
@@ -1,29 +1,29 @@
 ### Firefox changes ###
 
-## destativar cache de 5 abas recem visitadas
+## Destativar cache de 5 abas recem visitadas
 browser.sessionhistory.max_total_viewer
-    > -1 para 0
+    -1 para 0
 
 ## Mostrar url completa
 browser.urlbar.trimURLs
-    > true para false
+    true para false
 
 ## Selecionar toda url
 browser.urlbar.clickSelectsAll
-    > false para true
+    false para true
 
 ## Aumentar velocidade do scroll
 mousewheel.system_scroll_override_on_root_content.enabled
-    > false para true
+    false para true
 
 ## Associar links magnéticos (torrent) no Firefox
-    about:config
-    Nova entrada com o botão direito e acesse o menu: Nova referência → Boolean...
-    ## Como nome, coloque:
+    > about:config
+    nova entrada > nova referência > boolean > nome
     network.protocol-handler.expose.magnet
-    Quando perguntado se é TRUE ou FALSE, marque: FALSE
-    Na próxima vez que clicar num link magnético, aparecerá uma janela perguntando com que programa deseja abrir o link
-    Escolha o caminho: "/usr/bin/ktorrent" e marque para lembrar sempre dessa escolha
+    > false
+
+    Abra um link magnético, escolha o programa deseja abrir o link
+    > /usr/bin/ktorrent
 
 ## Habilitar o corretor ortográfico em todos os campos de texto
 layout.spellcheckDefault
@@ -33,45 +33,50 @@ layout.spellcheckDefault
 
 ## Remover mensagem "Tela cheia"
 full-screen-api.warning.delay
-    > 500 para 0
+    500 para 0
 
 full-screen-api.warning.timeout
-    > 3000 para 0
+    3000 para 0
 
 ## Definir zomm incial para arquivo pdf
 pdfjs.defaultZoomValue
-    > page-width
+    page-width
 
 ## Install ffmpeg to use video plugins
-# To test: https://www.youtube.com/html5
+    # To test: https://www.youtube.com/html5
 
-## Good add-ons
-    ## habilitado:
+## Bons complementos
+    ## Habilitado:
 https://adblockplus.org/
-    > Remove (crtl + shift + v) > about:config > extensions.adblockplus.sidebar_key
+    Remove (crtl + shift + v)
+        about:config
+            extensions.adblockplus.sidebar_key
+https://addons.mozilla.org/firefox/addon/disable-ctrl-q-shortcut/
+https://addons.mozilla.org/firefox/addon/gnotifier/
+    Notification engine - Standard
+    Default click action - Open folder
+    Notification expiration - Unmarked
+https://addons.mozilla.org/firefox/addon/google-br/
+    Change the search
+https://addons.mozilla.org/firefox/addon/inlinedisposition/
+
+    ## Desabilitado
+https://addons.mozilla.org/firefox/addon/grammarly-1/
+https://addons.mozilla.org/firefox/addon/flashgot/
+https://addons.mozilla.org/firefox/addon/flashstopper/
+http://en.savefrom.net/user.php?vid=387#download
 https://addons.mozilla.org/firefox/addon/adblock-plus-pop-up-addon/
     http://jessehakanen.net/adblockpluspopupaddon/#download
-    > Block all pop-ups
-https://addons.mozilla.org/firefox/addon/disable-ctrl-q-shortcut/
-https://addons.mozilla.org/firefox/addon/flashstopper/
-https://addons.mozilla.org/firefox/addon/gnotifier/
-    > Notification expiration
-https://addons.mozilla.org/firefox/addon/google-br/
-    > Change the search
-https://addons.mozilla.org/firefox/addon/flashgot/
-https://addons.mozilla.org/firefox/addon/grammarly-1/
-
-    ## desabilitado
+        Block all pop-ups
 http://www.baixou.com.br/baixouagora
 https://desprotetor.com.br/download
 https://addons.mozilla.org/firefox/addon/video-blocker/
 https://addons.mozilla.org/firefox/addon/kindle-it/
 https://addons.mozilla.org/firefox/addon/behind_the_overlay/
 https://addons.mozilla.org/firefox/addon/user-agent-switcher-firefox/
-https://addons.mozilla.org/firefox/addon/inlinedisposition/
 https://addons.mozilla.org/firefox/addon/video-downloadhelper/
 
-## others
+    ## Outros
 https://zenmate.com/
 https://www.eff.org/https-everywhere
 https://addons.mozilla.org/firefox/addon/private-tab/
@@ -82,7 +87,3 @@ https://addons.mozilla.org/firefox/addon/tab-notifier/
 https://addons.mozilla.org/firefox/addon/flagfox/
 https://addons.mozilla.org/firefox/addon/download-youtube/
 https://addons.mozilla.org/firefox/addon/download-flash-and-video/
-
-## New tab as blank
-browser.newtab.url
-    about:newtab > about:blank
