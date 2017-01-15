ccac82ff2a6e867ac1a854093ebe11cb489a8943
João Batista <joao42lbatista@gmail.com>
2017-01-15 10:14:54 -0200

    Up slackware_K45A_KDE.r

--- slackware_K45A_KDE.r
+++ slackware_K45A_KDE.r
@@ -1,6 +1,6 @@
-## Slackware_K45A_KDE.r to Slackware 14.2
+    ## Slackware_K45A_KDE.r to Slackware 14.2 ##
 
-## Editar
+## Edit
     nano /etc/lilo.conf
     nano /etc/fstab
     nano /etc/inittab
@@ -16,59 +16,22 @@
     nano /home/j/.config/akonadi/akonadiserverrc
     StartServer=true > StartServer=false
 
-## Desabilitar hibernação e dormir no kdesu (Hibernation)
-    kdesu kwrite /usr/share/polkit-1/actions/org.freedesktop.upower.policy
-    <allow_active>yes</allow_active> yes >> no
+## Disable Hibernation
+    nano /usr/share/polkit-1/actions/org.freedesktop.upower.policy
+
+        <action id="org.freedesktop.upower.hibernate">
+            <allow_active>yes</allow_active>
+                yes > no
 
 ## rc.local
-echo "/usr/bin/boot_rcLocal_JBs.sh" >> /etc/rc.d/rc.local
+    ## Download boot_rcLocal_JBs and copy to /usr/bin/
+    https://github.com/ryuuzaki42/5_scripts_slackware/blob/master/boot_rcLocal_JBs.sh
 
-    ####################### boot_rcLocal_JBs.sh #######################
-    ## Set brightness in 1%
-        #echo 0 > /sys/class/backlight/acpi_video0/brightness
-        ## or
-        #echo 50 > /sys/class/backlight/intel_backlight/brightness
-        ## or
-        usual_JBs.sh brigh-1
-
-    ## Set unicode
-        unicode_start
-
-    ## Set CPU performance. See the actual governor #cpufreq-info
-    ## http://docs.slackware.com/howtos:hardware:cpu_frequency_scaling
-        # See the count of CPU you have #cpufreq-info | grep "analyzing CPU"
-        cpufreq-set --cpu 0 --governor performance
-        cpufreq-set --cpu 1 --governor performance
-        cpufreq-set --cpu 2 --governor performance
-        cpufreq-set --cpu 3 --governor performance
-
-    ## Set brightness >= %1
-        /usr/bin/brightness_min_set_JBs.sh &
-
-    ## Print message by time
-        echo -e "\n\n"
-        echo -e "\t------------------"
-        echo -e "\t| Happy Day :-)! |"
-        echo -e "\t------------------"
-
-        hm=`date +%H%M`
-        hm=900
-        if [ $hm -lt 0601 ]; then
-            echo -e "\t| Boa Madrugada! |"
-        elif [ $hm -lt 1201 ]; then
-            echo -e "\t| Bom dia!       |"
-        elif [ $hm -lt 18001 ]; then
-            echo -e "\t| Boa tarde!     |"
-        else
-            echo -e "\t| Boa noite!     |"
-        fi
-        echo -e "\t------------------\n"
-        echo -e "\t `date`\n\n"
-    ####################### boot_rcLocal_JBs.sh #######################
+    ## Set to run at the startup
+    echo "/usr/bin/boot_rcLocal_JBs.sh" >> /etc/rc.d/rc.local
 
 ## Kernel Generic
-    link: https://www.vivaolinux.com.br/artigo/Guia-pos-instalacao-do-Slackware/?pagina=2
-        #/usr/share/mkinitrd/mkinitrd_command_generator.sh
+    # https://docs.slackware.com/slackware:beginners_guide#switch_to_a_generic_kernel
 
     /usr/share/mkinitrd/mkinitrd_command_generator.sh | grep mkinitrd | grep -v command | /bin/bash
 
@@ -77,7 +40,7 @@ echo "/usr/bin/boot_rcLocal_JBs.sh" >> /etc/rc.d/rc.local
     nano /etc/lilo.conf
     lilo
 
-## Touchpad de apenas uma parte/peça
+## Touchpad of just one part
     nano /etc/X11/xorg.conf
 
 Section "InputClass"
@@ -86,33 +49,35 @@ MatchDriver "synaptics"
 Option "SoftButtonAreas" "50% 0 82% 0 0 0 0 0"
 EndSection
 
-## Dolphin disable executable shellscript, perl, ruby, pyton
+## Dolphin disable executable shellscript, perl, ruby, pyton etc
     kdesu kwrite /usr/share/mime/packages/freedesktop.org.xml
 
     ## Look for the lines and comment
-        <sub-class-of type="application/x-executable"/> to <!-- <sub-class-of type="application/x-executable"/> --> # "
-        <sub-class-of type='application/x-executable'/> to <!-- <sub-class-of type='application/x-executable'/> --> # '
+        <sub-class-of type="application/x-executable"/>
+        <!-- <sub-class-of type="application/x-executable"/> -->
+
+        # And
+        <sub-class-of type='application/x-executable'/>
+        <!-- <sub-class-of type='application/x-executable'/> -->
+
+    ## Update Dolphin config
+        rm /usr/share/mime/packages/kde.xml
 
         update-mime-database /usr/share/mime
-
-    ## If unknown (Unknown media type in type ...)
-    rm /usr/share/mime/packages/kde.xml
-    update-mime-database /usr/share/mime
 
 ## Remove keyboard shortcut "Plasma Desktop Shell: Stop Current Activity"
     Is assigned meta (windows) + S
     System Settings > Shortcuts and Gestures > Global Keyboard Shortcuts
-    Select "Plasma Desktop Shell" in KDE component,
-    and in Action Remove the shortcut to "Stop Current Activity"
+    Select "Plasma Desktop Shell" in KDE component,and in Action Remove the shortcut to "Stop Current Activity"
 
-## Instalar
+## Install slackpkg
     slackpkg install flash-player-plugin p7zip unrar chrome
 
     slackpkg install icedtea-web libreoffice-5.* libreoffice-dict-en libreoffice-dict-pt-BR libreoffice-l10n-pt_BR libreoffice-kde-integration
 
-    ## Instalar cogroo e alterar idioma
+    ## libreoffice - install cogroo and change the language configuration
 
-## Local
+## Install local
     bleachbit dropbox virtualbox comix qpdf R redshift smplayer vlc openjdk
     # convmv pdftk pv
 
@@ -156,20 +121,24 @@ EndSection
     ## To install one package
         tlmgr install package
 
-## Remover games
-    slackpkg remove palapeli bomber granatier kblocks ksnakeduel kbounce kbreakout kgoldrunner kspaceduel kapman kolf kollision kpat lskat blinken khangman pairs ktuberling kdiamond ksudoku kubrick picmi bovo kblackbox kfourinline kmahjongg kreversi ksquares kigo kiriki kshisen gnuchess katomic kjumpingcube kmines knetwalk killbots klickety klines konquest ksirk knavalbattle kanagram amor kajongg
+## Remove games
+    ## Part 1
+    slackpkg remove palapeli bomber granatier kblocks ksnakeduel kbounce kbreakout kgoldrunner kspaceduel kapman kolf kollision kpat lskat blinken khangman pairs ktuberling
 
-## Remover servidor X
-    ## Remove all another X (leave KDE and XFCE)
-        slackpkg remove twm blackbox fluxbox windowmaker fvwm
+    ## Part 2
+    slackpkg remove kdiamond ksudoku kubrick picmi bovo kblackbox kfourinline kmahjongg kreversi ksquares kigo kiriki kshisen gnuchess katomic kjumpingcube kmines knetwalk
 
-    ## Leave fluxbox # Safe propose
-        slackpkg remove twm blackbox windowmaker fvwm
+    ## Part 3
+    slackpkg remove killbots klickety klines konquest ksirk knavalbattle kanagram amor kajongg
+
+## Remove desktop environments (X)
+    ## Remove all another X than KDE (leave fluxbox for safe propose)
+        slackpkg remove twm blackbox windowmaker fvwm xfce
 
     ## Set one by be the default
         xwmconfig
 
-## Remover kopote
+## Remove kopote
     slackpkg remove kdenetwork kdenetwork-filesharing kdenetwork-strigi-analyzers kopete
 
 ## Remove nepomuk
@@ -178,15 +147,22 @@ EndSection
 ## Remove akonadi
     slackpkg remove akonadi
 
+## Remove ktorrent
+    slackpkg remove ktorrent libktorrent
+
 ## Remove gnome "packages"
     slackpkg remove gcr polkit-gnome gnome-themes gnome-keyring libgnome-keyring
 
-## Outros
-    slackpkg remove seamonkey pidgin xchat dragon thunderbird kplayer calligra bluedevil blueman bluez bluez-firmware xine-lib xine-ui vim-gvim vim sendmail sendmail-cf xpdf tetex tetex-doc kget
+## Remove others
+    ## Part 1
+    slackpkg remove seamonkey pidgin xchat dragon thunderbird kplayer calligra bluedevil blueman bluez bluez-firmware
+
+    ## Part 1
+    slackpkg remove xine-lib xine-ui vim-gvim vim sendmail sendmail-cf xpdf tetex tetex-doc kget
 
     ## Dolphin need baloo baloo-widgets
 
-## Swap em arquivo
+## Swap in file
     dd if=/dev/zero of=/home/j/swapfile.img bs=1M count=8192 # 8 GiB = 8192
 
     dd if=/dev/zero of=/home/j/swapfile.img bs=1M count=6144 # 6 GiB = 6144
@@ -206,7 +182,7 @@ EndSection
     chmod 0600 /home/j/swapfile.img
     chown root /home/j/swapfile.img
 
-## Ver valor de swappiness atual
+## See the actual value of swappiness
     cat /proc/sys/vm/swappiness
 
     ## To temporarily set the swappiness value
@@ -219,8 +195,9 @@ EndSection
     sysctl –p
 
 ## Move windows between Desktop
-    # Add 4 Desktop
+    ## Add 4 Desktop
 
+    ## Add shortcuts
     System Settings > Shortcuts and Gestures >  Global Keyboard Shortcuts
     Selecting "KWin"
     Search for "one"
@@ -237,7 +214,7 @@ EndSection
 
 ## Blacklist (slackpkg)
     nano /etc/slackpkg/blacklist
-#
+
 ffmpeg
 kdenlive
 ladspa_sdk
@@ -252,20 +229,15 @@ wxPython
 x264
 xerces-c
 bleachbit
-#glib2
-#xf86-video-intel
-#glib
-#
+glib2
+xf86-video-intel
+glib
 
-## slackpkgplus.config
+## slackpkgplus.config others mirrors
 nano /etc/slackpkg/slackpkgplus.conf
 
-# Slackware 14.1 - x86_64
-#MIRRORPLUS['multilib']=http://taper.alienbase.nl/mirrors/people/alien/multilib/14.1/
-#MIRRORPLUS['alienbob']=http://taper.alienbase.nl/mirrors/people/alien/sbrepos/14.1/x86_64/
-MIRRORPLUS['alienbob']=http://slackware.uk/people/alien/sbrepos/14.1/x86_64/
-#MIRRORPLUS['restricted']=http://taper.alienbase.nl/mirrors/people/alien/restricted_sbrepos/14.1/x86_64/
-MIRRORPLUS['restricted']=http://slackware.uk/people/alien/restricted_sbrepos/14.1/x86_64/
-#MIRRORPLUS['slacky']=http://repository.slacky.eu/slackware64-14.1/
-MIRRORPLUS['rlworkman']=http://rlworkman.net/pkgs/14.1/x86_64/
-#
+MIRRORPLUS['multilib']=http://slackware.uk/people/alien/multilib/14.2/
+MIRRORPLUS['alienbob']=http://slackware.uk/people/alien/sbrepos/14.2/x86_64/
+MIRRORPLUS['restricted']=http://slackware.uk/people/alien/restricted_sbrepos/14.2/x86_64/
+MIRRORPLUS['slacky']=http://repository.slacky.eu/slackware64-14.2/
+MIRRORPLUS['rlworkman']=http://rlworkman.net/pkgs/14.2/x86_64/
