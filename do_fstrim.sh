582f2d95e226383d9bde34a4a3b1d1c29909b6d2
João Batista <joao42lbatista@gmail.com>
2017-06-14 21:49:06 -0300

    # do_fstrim.sh - Add To fstrim in the SSD partitions

---
+++ do_fstrim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOGFILE=/var/log/ssd_trim.log
+SSD_MOUNT='/ /media/sda2/' # Chane for you folder where you mount the SSD partitions
+
+[ ! -e $LOGFILE ] && touch $LOGFILE
+
+echo "*** $(date -R) ***" | tee -a $LOGFILE
+
+for MNT in $SSD_MOUNT; do
+    fstrim -v $MNT | tee -a $LOGFILE
+done
+
+echo "    -------------------------------" | tee -a $LOGFILE
