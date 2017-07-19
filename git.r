a9349ad69fa858821490462cdc78d9c5c2b70e8c
João Batista <joao42lbatista@gmail.com>
2017-07-19 01:05:47 -0300

    git.r - Add commands

--- git.r
+++ git.r
@@ -202,3 +202,40 @@
     #comandGitRun="git gc"; for folderGit in `ls`; do echo -e "\nRunning \"git gc\" inside: \"$folderGit/\"\n"; cd $folderGit; $comandGitRun; cd ..; done
 
     for folderGit in `ls`; do echo -e "\nRunning \"git gc\" inside: \"$folderGit/\"\n"; cd $folderGit; git gc; cd ..; done
+
+## Show all users and the number of commits
+    git shortlog -sn
+
+## Changing author info - https://help.github.com/articles/changing-author-info/
+    ## 1 - Create a fresh, bare clone of your repository:
+        git clone --bare https://github.com/user/repo.git
+        cd repo.git
+
+    ## 2 Copy and paste the script, replacing the following variables based on the information you gathered:
+        OLD_EMAIL
+        CORRECT_NAME
+        CORRECT_EMAIL
+
+        #!/bin/sh
+        git filter-branch --env-filter '
+        OLD_EMAIL="your-old-email@example.com"
+        CORRECT_NAME="Your Correct Name"
+        CORRECT_EMAIL="your-correct-email@example.com"
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_COMMITTER_NAME="$CORRECT_NAME"
+            export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_AUTHOR_NAME="$CORRECT_NAME"
+            export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        fi
+        ' --tag-name-filter cat -- --branches --tags
+
+    ## 3 Push the corrected history to GitHub
+        git push --force --tags origin 'refs/heads/*'
+
+    ## 4 Clean up the temporary clone:
+        cd ..
+        rm -rf repo.git
