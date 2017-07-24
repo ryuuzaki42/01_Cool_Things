79b2866a33718eadcc754d4b7e4bbe2903efccbc
João Batista <joao42lbatista@gmail.com>
2017-07-24 13:40:34 -0300

    linkGenerate_gitClone_JBs.sh - "FOlder" to "Folder"

--- linkGenerate_gitClone_JBs.sh
+++ linkGenerate_gitClone_JBs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-gitCloneFOlder="/media/sda2/prog/git_clone"
+gitCloneFolder="/media/sda2/prog/git_clone"
 dropboxFolder="/media/sda2/prog/Dropbox"
 
-ln -s $gitCloneFOlder/1_cool_dark_things/ $dropboxFolder/0_pessoal/
+ln -s $gitCloneFolder/1_cool_dark_things/ $dropboxFolder/0_pessoal/
 
-ln -s $gitCloneFOlder/3_dissertation_tex/ $dropboxFolder/mestrado/dissertacao/
+ln -s $gitCloneFolder/3_dissertation_tex/ $dropboxFolder/mestrado/dissertacao/
 
-ln -s $gitCloneFOlder/4_paper_01_mestrado/ $dropboxFolder/mestrado/dissertacao/
+ln -s $gitCloneFolder/4_paper_01_mestrado/ $dropboxFolder/mestrado/dissertacao/
 
-ln -s $gitCloneFOlder/7_SUMO_OMNeT_Veins_Slackware $dropboxFolder/mestrado/
+ln -s $gitCloneFolder/7_SUMO_OMNeT_Veins_Slackware $dropboxFolder/mestrado/
 
-ln -s $gitCloneFOlder/8_veins_simulation/ $dropboxFolder/mestrado/dissertacao/
+ln -s $gitCloneFolder/8_veins_simulation/ $dropboxFolder/mestrado/dissertacao/
 
-ln -s $gitCloneFOlder/9_cluster_icmc_cemeai $dropboxFolder/mestrado/dissertacao/
+ln -s $gitCloneFolder/9_cluster_icmc_cemeai $dropboxFolder/mestrado/dissertacao/
