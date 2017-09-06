9f117911c4d7af167163a87d262ede733edcfc5c
João Batista <joao42lbatista@gmail.com>
2017-09-06 12:27:55 -0300

    shortcuts_common.r - Add

---
+++ shortcuts_common.r
@@ -0,0 +1,54 @@
+## Text-Editing Keyboard Shortcuts That Work Almost Everywhere
+
+## Working With Words
+Ctrl + Left or Right Arrow Keys - Move cursor between words
+Ctrl + Backspace - Delete previous word
+Ctrl + Delete - Delete next word
+Ctrl + Up Arrow - Move cursor to beginning of paragraph
+Ctrl + Down Arrow - Move cursor to end of paragraph
+
+## Moving the Cursor
+Home - Move cursor to beginning of current line
+End - Move cursor to end of current line
+Ctrl + Home - Move cursor to top of the text entry field
+Ctrl + End - Move cursor to bottom of the text entry field
+Page Up - Move cursor up a frame
+Page Down - Move cursor down a frame
+
+## Selecting Text
+Shift + Left or Right Arrow Keys - Select characters one at a time
+Shift + Up or Down Arrow Keys - Select lines one at a time
+Shift + Ctrl + Left or Right Arrow Keys - Select words
+Shift + Ctrl + Up or Down Arrow Keys - Select paragraphs
+Shift + Home - Select the text between the cursor and the beginning of the current line
+Shift + End - Select the text between the cursor and the end of the current line
+Shift + Ctrl + Home - Select the text between the cursor and the beginning of the text entry field
+Shift + Ctrl + End - Select the text between the cursor and the end of the text entry field
+Shift + Page Down - Select a frame of text below the cursor
+Shift + Page Up - Select a frame of text above the cursor
+Ctrl + A - Select all text
+
+## Editing
+Ctrl + C - Copy selected text
+Ctrl + X - Cut selected text
+Ctrl + V - Paste text at cursor
+Ctrl + Z - Undo
+Ctrl + Y - Redo
+
+## Formatting
+Ctrl + B - Bold
+Ctrl + I - Italic
+Ctrl + U - Underline
+
+## Functions
+Ctrl + F - The find dialog
+F3 - Find next
+Shift + F3 - Find previous
+Ctrl + O - Open
+Ctrl + S - Save
+Ctrl + N - New document
+Ctrl + P - Print
+Alt - Activate application’s menu bar
+Alt + F - Open File menu
+Alt + E - Open Edit menu
+Alt + V - Open View menu
