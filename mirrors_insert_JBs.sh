580faeaaf3299e0f6ccdbcbddf0a0a145ea14d52
João Batista <joao42lbatista@gmail.com>
2017-10-27 19:26:23 -0200

    mirrors_insert_JBs.sh - add date and set ftp as default mirror

--- mirrors_insert_JBs.sh
+++ mirrors_insert_JBs.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Insert text of mirror in the begin of /etc/slackpkg/mirrors
+#
+# Last update: 26/10/2017
+#
 echo -en "\nThis script insert in the begin of /etc/slackpkg/mirrors"
 echo -en " the message below about mirrors.\nWant continue? (y)es or (n)o: "
 read -r continueOrNot
@@ -14,10 +38,10 @@ if [ "$continueOrNot" == 'y' ]; then
 #cdrom://media/dvd/
 #-----------------------------------------------------------------------------#
 # Local Directory
-file://media/sda2/prog/git_clone/2_clone_Slackware_repo/slackware64-14.2/
+#file://media/sda2/prog/git_clone/2_clone_Slackware_repo/slackware64-14.2/
 #-----------------------------------------------------------------------------#
 # UNITED STATES (US)
-#ftp://ftp.osuosl.org/.2/slackware/slackware64-14.2/
+ftp://ftp.osuosl.org/.2/slackware/slackware64-14.2/
 #-----------------------------------------------------------------------------#
 # BRAZIL (BR)
 #ftp://ftp.slackware-brasil.com.br/slackware64-14.2/
