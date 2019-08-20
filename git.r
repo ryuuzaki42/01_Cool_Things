3d55b15083655facbfdd3302c98dbb4150a44f4b
João Batista <joao42lbatista@gmail.com>
2019-08-20 15:06:06 -0300

    git.r - "Describe how you plan to use GitHub"

--- git.r
+++ git.r
@@ -1,5 +1,29 @@
 ## Git commands ##
 
+## Describe how you plan to use GitHub
+    # https://www.quora.com/How-do-you-plan-to-use-GitHub
+
+    I aim to make use of Github in the following ways:
+
+    1 Showcase my work: Github is a great way for me to showcase my work to the world, and to allow others to contribute to my work as well.
+    The world, especially in tech, is moving towards emphasizing people’s skills and their abilities over academic credentials when it comes
+    to recruiting, so it’s a great way for me to showcase my real-world coding experience.
+
+    2 Make open-source contributions: There are a lot of cool and impactful open-source contributions on Github. I aspire to contribute to
+    open-source projects in areas of my interest, which are currently machine learning and statistics. This is not only a great way for me to
+    hone my skills, but is also a way for me to push my field of interest forward. For example, I want to contribute to Python packages such as
+    scikit-learn (for Machine Learning) and statsmodels (for Statistics).
+
+    3 Collaborate with project partners: Github is great for collaborating with others on my projects. For eg, if a bunch of friends and I are working
+    on a side project, I’d want to use Github to collaborate with them. It’s very easy to synchronize progress using Github when working with a team.
+
+    4 Access interesting but less well-known open-source contributions: There are a lot of nascent open-source contributions that have proven to be
+    immensely useful in my research and side projects. Examples are: a sentence2vec implementation, and a tool to create interactive t-SNE visualizations
+
+    5 Create impactful open-source projects: I strongly aspire to create high impact open-source projects in my areas of interest. For example, it is my
+    ambition to create an equivalent (and hopefully, even better version) of ggplot (a wonderful visualization package for R) in Python, which would be
+    extremely useful for data scientists who work primarily in Python. (Note: If you are inspired to work on such a project as well, please do let me know).
+
 ## Good links
     http://git-scm.com/book/
     https://leanpub.com/pro-git/read
