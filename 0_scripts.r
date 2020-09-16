c3a4d3dc9d8a2a9e3be48f1c053a0cd16097dbba
João Batista <joao42lbatista@gmail.com>
2020-09-16 10:12:14 -0300

    0_scripts.r - add print network connections, ps commands, see the size of directories and Sort numbers values - separated by a dot or any other separator character

--- 0_scripts.r
+++ 0_scripts.r
@@ -1618,3 +1618,62 @@ fi
 
     # URL:
     http://localhost/infophp.php
+
+## Print network connections
+    netstat -atunp
+
+## ps commands
+
+    ## To see every process on the system using standard syntax:
+        ps -e
+        ps -ef
+        ps -eF
+        ps -ely
+
+    ## To see every process on the system using BSD syntax:
+        ps ax
+        ps axu
+
+    ## To print a process tree:
+        ps -ejH
+        ps axjf
+
+    ## To get info about threads:
+        ps -eLf
+        ps axms
+
+    ## To get security info:
+        ps -eo euser,ruser,suser,fuser,f,comm,label
+        ps axZ
+        ps -eM
+
+    ## To see every process running as root (real & effective ID) in user format:
+        ps -U root -u root u
+
+    ## To see every process with a user-defined format:
+        ps -eo pid,tid,class,rtprio,ni,pri,psr,pcpu,stat,wchan:14,comm
+        ps axo stat,euid,ruid,tty,tpgid,sess,pgrp,ppid,pid,pcpu,comm
+        ps -Ao pid,tt,user,fname,tmout,f,wchan
+
+    ## Print only the process IDs of syslogd:
+        ps -C syslogd -o pid=
+
+    ## Print only the name of PID 42:
+        ps -q 42 -o comm=
+
+## See the size of directories
+    du -sh *
+
+## Sort numbers values - separated by a dot or any other separator character
+
+echo "1.0.0
+1.0.0.1018
+1.0.0.1019
+1.0.0.1019
+1.0.0.7" | sort --version-sort --field-separator=.
+
+echo "1.0.0
+1.0.0.1018
+1.0.0.1019
+1.0.0.1019
+1.0.0.7" | sort --version-sort --field-separator=. --key=4
