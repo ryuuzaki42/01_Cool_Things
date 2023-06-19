2e05c443389bfc4c1d9dea52ea20c4d6704ba0b0
João Batista <joao42lbatista@gmail.com>
2023-06-19 16:59:58 -0300

    rc.local_JBs.sh - \\n to \n \\b to \n \\t to \t

--- slackware_config/rc.local_JBs.sh
+++ slackware_config/rc.local_JBs.sh
@@ -7,34 +7,34 @@
 # make an /etc/rc.d/rc.local_shutdown script and put those
 # commands in there.
 # - JB cs -
-# Last update: 02/06/2023
+# Last update: 19/06/2023
 #
 if [ -x /etc/rc.d/rc.tlp ]; then
-    echo -e "\\n # Starting tlp #\\n"
+    echo -e "\n # Starting tlp #\n"
     # https://github.com/linrunner/TLP
     /etc/rc.d/rc.tlp start
 fi
 
-echo -e "\\n # Disabling Bluetooth #\\n"
+echo -e "\n # Disabling Bluetooth #\n"
 rfkill block bluetooth
 
 if [ -x /etc/rc.d/rc.thinkfan ]; then
-    echo -e "\\n # Starting thinkfan #\\n"
+    echo -e "\n # Starting thinkfan #\n"
     # https://github.com/vmatare/thinkfan
     /etc/rc.d/rc.thinkfan start
 fi
 
 # Load the keyboard map. More maps are in /usr/share/kbd/keymaps/
 # /usr/share/kbd/keymaps/i386/qwerty/br-abnt2.map.gz
-echo -e "\\n # Set keyboard layout to BR abnt2 - loadkeys br-abnt2 #\\n"
+echo -e "\n # Set keyboard layout to BR abnt2 - loadkeys br-abnt2 #\n"
 loadkeys br-abnt2
 
-#echo -e "\\n # Starting Teamviewer #\\n"
+#echo -e "\n # Starting Teamviewer #\n"
 #if [ -x /etc/rc.d/rc.teamviewerd ]; then
 #    /etc/rc.d/rc.teamviewerd start
 #fi
 
-#echo -e "\\n # Update time - Notebook without battery #\\n"
+#echo -e "\n # Update time - Notebook without battery #\n"
 #count=0
 #while [ "$count" -lt 10 ]; do
 #    result_date_up=$(usual_JBs.sh date-up)
@@ -48,16 +48,16 @@ loadkeys br-abnt2
 #done
 #echo "$result_date_up"
 
-#echo -e "\\n # Starting boot_rcLocal_JBs.sh #\\n"
+#echo -e "\n # Starting boot_rcLocal_JBs.sh #\n"
 #/usr/bin/boot_rcLocal_JBs.sh
 
 ## If your backlight keyboard doesn't work
-#echo -e "\\n # Starting UPower #\\n"
+#echo -e "\n # Starting UPower #\n"
 #qdbus --system org.freedesktop.UPower
 
 ## If you have installed NVIDIA video driver with bumblebeed
 #if [ -x /etc/rc.d/rc.bumblebeed ]; then
-#    echo -e "\\n # Starting bumblebeed #\\n"
+#    echo -e "\n # Starting bumblebeed #\n"
 #    /etc/rc.d/rc.bumblebeed start
 #fi
 
