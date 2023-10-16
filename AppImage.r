4987653c26a60599c92d6dbb353d092a7138a757
João Batista <joao42lbatista@gmail.com>
2023-10-16 04:53:27 -0300

    Update AppImage.r

--- AppImage.r
+++ AppImage.r
@@ -7,68 +7,74 @@
 # Mande me um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Last update: 08/08/2023
+# Last update: 16/10/2023
 #
 
 https://github.com/AppImage/AppImageKit/blob/master/README.md
 
-## Extract AppImage - --appimage-extract
+## Extract AppImage
+    --appimage-extract
+
     ./Prog.AppImage --appimage-extract
 
-## Mount AppImage - --appimage-mount
+## Mount AppImage
+    --appimage-mount
+
     ./Prog.AppImage --appimage-mount
 
+## File name is defined in:
+    squashfs-root/Prog.desktop
+
+    squashfs-root/WinRAR.desktop
+        Name=winrar-6.24
+
 ## Create AppImage
-    # https://github.com/AppImage/AppImageKit/releases/latest
-    ## Download
-        wget https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
-        chmod +x appimagetool-x86_64.AppImage
+    https://github.com/AppImage/AppImageKit/releases
 
-    ## Version
+    ## Last version
         ./appimagetool-x86_64.AppImage --version
-            appimagetool, continuous build (commit 8bbf694), build <local dev build> built on 2020-12-31 11:48:33 UTC
+            appimagetool, continuous build (commit 5735cc5), build <local dev build> built on 2023-03-08 22:52:04 UTC
 
-    ## File name is defined in: - squashfs-root/Prog.desktop
-        squashfs-root/WinRAR.desktop
-            Name=winrar-6.23
-
-    ## Make AppImage - ARCH=
+    ## 32 bits
         ARCH=i686 ./appimagetool-x86_64.AppImage squashfs-root/
 
+    ## 64 bits
         ARCH=x86_64 ./appimagetool-x86_64.AppImage squashfs-root/
 
         ARCH=x86_64 ./appimagetool/appimagetool-x86_64.AppImage squashfs-root/
 
-## Try extract .exe program
-    ## Check if has one portable version
+## Extract .exe program
+    1 Check if has one portable version
 
-    ## Rename the .exe to .rar or .zip and try extract with ark or PeaZip*.AppImage
+    2 Rename the .exe to .rar or .zip and try extrart with ark or PeaZip*.AppImage
 
-    ## Install the program in the Windows and copy the install folder
+    3 Install the program in the Windows and copy the install folder
 
-## WinRAR AppImage
+## WinRAR
     ## Rename .exe to .rar
         mv winrar-x64-623.exe winrar-x64-623.rar
 
-        ## Extract WinRAR*.rar
+    ## Extract WinRAR*.rar
 
     ## Clone mirror
         git clone https://github.com/ryuuzaki42/WinRAR_AppImage
 
-        ## Upgrade WinRAR install in WinRAR_AppImage/drive_c/WinRAR/
+    ## Upgrade WinRAR install in WinRAR_AppImage/drive_c/WinRAR/
 
-            ## Make commit of update version
+    ## Make commit of update / version
+
+    ## Github actions auto generate the AppImage
 
         ## Move to trash
             .git/
             .github/
             .gitignore
 
-            ## Key
-                drive_c/WinRAR/rarreg.key
+        ## Add Key
+            drive_c/WinRAR/rarreg.key
 
-    ## Create AppImage
-        ARCH=x86_64 ./appimagetool-x86_64.AppImage WinRAR_AppImage/
+        ## Create AppImage
+            ARCH=x86_64 ./appimagetool-x86_64.AppImage WinRAR_AppImage/
 
 ## Command line arguments
     --appimage-help
@@ -78,9 +84,9 @@ https://github.com/AppImage/AppImageKit/blob/master/README.md
         Prints the offset at which the embedded filesystem image starts, and then exits.
         This is useful in case you would like to loop-mount the filesystem image using the mount -o loop,offset=... command
 
-    --appimage-extract
+    --appimage-extract [<pattern>]
         Extracts the contents from the embedded filesystem image, then exits.
-        [<pattern>] If pattern is passed, only extract matching files
+        If pattern is passed, only extract matching files
         This is useful if you are using an AppImage on a system on which FUSE is not available
 
     --appimage-mount
@@ -88,22 +94,35 @@ https://github.com/AppImage/AppImageKit/blob/master/README.md
         This is useful if you would like to inspect the contents of an AppImage without executing the contained payload application
 
     --appimage-version
-        prints the version of AppImageKit, then exits. This is useful if you would like to file issues
+        Prints the version of AppImageKit, then exits. This is useful if you would like to file issues
 
     --appimage-updateinformation
-        prints the update information embedded into the AppImage, then exits.
+        Prints the update information embedded into the AppImage, then exits.
         This is useful for debugging binary delta updates
 
     --appimage-signature
-        prints the digital signature embedded into the AppImage, then exits.
+        Prints the digital signature embedded into the AppImage, then exits.
         This is useful for debugging binary delta updates. If you would like to validate the embedded signature,
         you should use the validate command line tool that is part of AppImageKit
 
     --appimage-portable-home
         Create a portable home folder to use as $HOME
 
-  --appimage-portable-config]
+    --appimage-portable-config
         Create a portable config folder to use as $XDG_CONFIG_HOME
 
-  --appimage-updateinfo
+    --appimage-updateinfo
         Print update info embedded in AppImage
+
+    ## Portable home:
+        If you would like the application contained inside this AppImage to store its
+        data alongside this AppImage rather than in your home directory, then you can
+        place a directory named
+
+        <PATH_to_App>/APP_FULL_Name.AppImage.home/
+        /media/sda2/os/gnu_linux/slackware15.0/AppImage/appimagetool/appimagetool-x86_64.AppImage.home
+
+        Or you can invoke this AppImage with the --appimage-portable-home option,
+        which will create this directory for you. As long as the directory exists
+        and is neither moved nor renamed, the application contained inside this
+        AppImage to store its data in this directory rather than in your home directory
