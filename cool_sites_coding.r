77afad30c602bee7d30319058ea0df48a9fad41b
João Batista <joao42lbatista@gmail.com>
2023-10-19 07:49:29 -0300

    Update cool_sites_coding.r

--- cool_sites_coding.r
+++ cool_sites_coding.r
@@ -1,21 +1,35 @@
+
 ## Cool sites for coding
 
 ## Hackerrank
-https://www.hackerrank.com/
+    https://www.hackerrank.com/
 
 ## URI Online Judge - Problems & Contests
-https://www.urionlinejudge.com.br/
+    https://www.urionlinejudge.com.br/
+
+    ## Changed to beecrowd
+        https://www.beecrowd.com.br/
 
 ## Interview Bit
-https://www.interviewbit.com/
+    https://www.interviewbit.com/
 
 ## Code Fights
-https://codefights.com/
+    https://codefights.com/
 
-## Live Coding
-https://www.livecoding.tv/
+## Online compiler
+    https://www.programiz.com/c-programming/online-compiler/
+    https://replit.com/
+    https://www.onlinegdb.com/
+    https://www.programiz.com/html/online-compiler/
+    https://www.programiz.com/python-programming/online-compiler/
+
+## Visualization of data structures and algorithms
+    https://csvistool.com/
+    https://www.cs.usfca.edu/~galles/visualization/Algorithms.html
+    https://visualgo.net/
+    https://algorithm-visualizer.org/
 
 ## Fun Sites
-https://www.nayuki.io/
-https://nihey.org/
-https://try.github.io/
+    https://www.nayuki.io/
+    https://nihey.org/
+    https://try.github.io/
