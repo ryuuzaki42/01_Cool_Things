b69a7a2032a8b89ae882f111623ecdadd6e59ab0
João Batista <joao42lbatista@gmail.com>
2024-01-05 14:58:32 -0300

    add LibreOffice.r

---
+++ LibreOffice.r
@@ -0,0 +1,33 @@
+
+## LibreOffice Writer - Make typing cursor more visible in Writer
+    # LibreOffice Writer - Typing cursor (caret, not the mouse pointer) appearance
+
+    ~/.config/gtk-3.0/gtk.css
+
+    ## Add
+        * { -GtkWidget-cursor-aspect-ratio: 0.2; }
+
+## LibreOffice without icons
+    Switch to another icon style in the Tools > Option > Libreoffice > View > Icon Menu, for example 'tango'
+
+## LibreOffice: Colocar ponto (.) no lugar de virgula (,) para separar casas decimais
+    Mude o Local setting: English (UK)
+
+## Calc
+
+    ## Export sheet to PDF
+        ## Change page style before export
+
+            Format > Page Style
+            Format > Page Style
+                > Page
+                    > Orientation
+                        > Portrait or Landscape
+
+                > Header
+                    > Disable or change some configuration
+                    ## Disable to remove sheet name
+
+                > Footer
+                    > Disable or change some configuration
+                    ## Disable to remove page name
