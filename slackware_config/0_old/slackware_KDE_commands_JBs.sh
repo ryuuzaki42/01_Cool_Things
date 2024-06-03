a31a84be1fcff2163602e021c7592efe3456852f
João Batista <joao42lbatista@gmail.com>
2024-06-03 09:18:11 -0300

    mirrors_insert_JBs.sh and slackware_KDE_commands_JBs.sh - "Mande me" to "me envie"

--- slackware_config/0_old/slackware_KDE_commands_JBs.sh
+++ slackware_config/0_old/slackware_KDE_commands_JBs.sh
@@ -2,7 +2,7 @@
 #
 # Autor= João Batista Ribeiro
 # Bugs, Agradecimentos, Críticas "construtivas"
-# Mande me um e-mail. Ficarei Grato!
+# me envie um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
 # Este programa é um software livre; você pode redistribui-lo e/ou
