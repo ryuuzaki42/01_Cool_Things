35a16dea8c5da466e421b0656068a5bed7f37868
João Batista <joao42lbatista@gmail.com>
2024-07-25 20:26:26 -0300

    vs_code.r add

---
+++ vs_code.r
@@ -0,0 +1,47 @@
+
+## VS code - VS codium #
+
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Last update: 25/07/2024
+#
+
+# Download
+    https://code.visualstudio.com/Download
+
+    ## VS codium - AppImage
+    https://github.com/VSCodium/vscodium/releases
+
+## Shotcuts
+    Command Palette (Ctrl + Shif + P)
+    Extensions (Ctrl + Shif + X)
+    Settings  (Ctrl + ,)
+
+## Python
+    Install extension:
+        Python (Python and Python Debugger)
+        Pylint - To see code error and suggestions
+
+    ## Jupyter notebook
+        Install extension:
+            Jupyter (Jupyter, Jupyter Cell Tags, Jupyter Keymap, Jupyter Notebook Renderers, Jupyter Slideshow)
+
+    # Selecione ambiente virutal para execução
+        Ctrl + Shift + P
+            > Python: Select Interpreter
+                Selecione o caminho para my_env/bin/Python3
+
+        Depois altere o Kernel em Select Kernel
+            Selecione o Interpreter adicionado no passo anterior
+## C
+    Install extension:
+        clangd (C/C++ completion, navigation, and insights)
+
+## Indent code
+    Windows Shift + Alt + F
+    Mac     Shift + Option + F
+    Linux   Ctrl + Shift + I
