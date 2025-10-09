7193fe501c036f2ee4620324851a83335cd0b039
João Batista <joao42lbatista@gmail.com>
2025-10-09 16:44:55 -0300

    Update other_browsers.r

---
+++ other_browsers.r
@@ -0,0 +1,25 @@
+
+    ## other_browsers ##
+
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Last update: 11/09/2025
+#
+
+    ## Vivaldi ##
+
+## Vivaldi freeze after update - AppImage
+    ## Remove Bookmarks and restart, after can restore the Bookmarks
+    cd ~/.config/vivaldi/Default/
+
+    mv Bookmarks Bookmarks2
+
+    ## Start Vivaldi and close
+
+    mv Bookmarks2 Bookmarks
+
+-------------------------------------------------------------------------------
