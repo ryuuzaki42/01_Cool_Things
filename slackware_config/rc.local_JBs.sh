0267e5d8f5d87ed44e64e7a5a1fd3d8c9af0478f
João Batista <joao42lbatista@gmail.com>
2025-10-12 10:41:42 -0300

    Update rc.local_JBs.sh

--- slackware_config/rc.local_JBs.sh
+++ slackware_config/rc.local_JBs.sh
@@ -15,10 +15,14 @@ disable_bluetooth=1 #1
 start_thinkfan=1 #1
 
 set_keyboard=1 #1 # Load the keyboard map
-keyboard_map="br-abnt2" # Change to your desire map. More maps are in /usr/share/kbd/keymaps/
+keyboard_map="br-abnt2" # Change to the desire map. More maps in: /usr/share/kbd/keymaps/
 
 update_date=0 #0 # Need usual_JBs.sh - https://github.com/ryuuzaki42/02_Scripts_Linux/
-use_tmp_folder_RAM=1 #1
+
+use_tmp_folder_RAM=1 #1 # Mount a temporary folder to RAM
+tmp_folder_RAM="/media/sda2/home/j/Downloads/0_tmp_folder_RAM" # Change to the desired folder
+normal_user="j" # Change to our "normal" user ID/name
+
 disable_touchpad=0 #0
 
 if [ "$start_tlp" == 1 ]; then
@@ -63,8 +67,6 @@ fi
 
 if [ "$use_tmp_folder_RAM" == 1 ]; then
     echo -e "\n # Mount a temporary folder to RAM #"
-    tmp_folder_RAM="/media/sda2/home/j/Downloads/0_tmp_folder_RAM" # Change to your folder
-    normal_user="j"
     mkdir "$tmp_folder_RAM" 2> /dev/null
 
     # Grep count of RAM in MiB - some computers use part of RAM to GPU
