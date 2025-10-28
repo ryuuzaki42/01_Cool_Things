c36cdc033d54f0241adc333aeb8dc7a212b007df
João Batista <joao42lbatista@gmail.com>
2025-10-28 15:19:52 -0300

    Update 0_check_config_files_JBs.sh

--- slackware_config/0_check_config_files_JBs.sh
+++ slackware_config/0_check_config_files_JBs.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Check Slackware configurations files are equal with the system
+#
+# Last update: 15/12/2025
+#
 check_file() {
     command_run="$program $1 $2"
     echo -e "\n # $command_run #"
