35b9862c3316d15a04b00e8a308ebb25cd4038cc
João Batista <joao42lbatista@gmail.com>
2026-05-08 13:44:08 -0300

    slackware_15.0_kernel_upgrade.r - Added

---
+++ slackware_config/slackware_15.0_kernel_upgrade.r
@@ -0,0 +1,75 @@
+
+    ## Slackware_15.0_kernel_upgrade #
+
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Last update: 08/05/2026
+#
+
+## 1 Generate new initrd
+    Rebuilt initial ramdisck - /boot/initrd.gz
+
+    pkgtool
+        > Setup
+            > 01.mkinitrd
+
+    ## Or
+        /usr/share/mkinitrd/mkinitrd_command_generator.sh -k "kernel" | bash
+
+## 2 Update lilo
+    > lilo
+
+## 3 VirtualBox
+    /sbin/vboxconfig
+
+    ls -l /etc/rc.d/init.d/vbox*
+
+    chmod -x /etc/rc.d/init.d/vbox*
+
+    ls /lib/modules/
+
+    rm -r /lib/modules/OLD_Version
+
+## 4 Nvidia-kernel pkg
+    0imp/14_Nvidia_Driver_Slackware/final_packages/
+        # nvidia-driver-*_multilib-x86_64-1_JB.txz - not need
+
+        nvidia-kernel-*_"kernel"-x86_64-1_JB.txz
+
+## Clean system
+    ## As root
+        clean_tmp_folder_JBs.sh all
+
+    ## As normal user
+        clean_tmp_folder_JBs.sh all
+
+    ## Remove old packages logs
+        rm_old_logs_JBs.sh
+
+    ## Trim SSD
+        /etc/cron.weekly/fstrim_JBs.sh
+
+---------------------------------------------------------------------
+
+## 5 acpi_call pkg
+    # https://github.com/teleshoes/acpi_call
+    # https://github.com/nix-community/acpi_call/tags
+
+    ## Not need
+        1txz/TLP/need/
+            acpi_call-1.2.2_"kernel"-x86_64-1_JB.txz
+
+    tlp-stat -b
+
+## 6 v4l2loopback
+    OBS - Create "virtual video devices", in OBS Virtual Camera for example
+    droidcam - need to work
+
+    After install, need restart to start to working
+
+    # https://slackbuilds.org/repository/15.0/system/v4l2loopback/
+        v4l2loopback-0.13.2_"kernel"-x86_64-1_JB.txz
