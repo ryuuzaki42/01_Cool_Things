b5d93677703a1739282e5974fdb8758bfde472b7
João Batista <joao42lbatista@gmail.com>
2026-07-08 15:20:37 -0300

    Update rc.local_shutdown_JBs.sh

---
+++ slackware_config/rc.local_shutdown_JBs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# /etc/rc.d/rc.local_shutdown:  Local system shutdown script.
+#
+# Put any commands in here that needs to be run at shutdown time
+#
+# - JB cs -
+# Last update: 08/07/2026
+#
+# 1 is true or will run and 0 to false
+clean_system=1 #1 # Need cleanup_system_JBs - https://github.com/ryuuzaki42/02_Scripts_Linux/
+
+if [ "$clean_system" == 1 ]; then
+    echo -e "\n # Running cleanup_system_JBs all y n #"
+    cleanup_system_JBs all y n
+fi
+
+# - JB ce -
