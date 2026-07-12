f8c25e030693e871487208bcdf44bd9e567bc212
João Batista <joao42lbatista@gmail.com>
2026-07-12 10:22:46 -0300

    Update rc.local_shutdown_JBs.sh

--- slackware_config/rc.local_shutdown_JBs.sh
+++ slackware_config/rc.local_shutdown_JBs.sh
@@ -9,15 +9,18 @@
 #
 # 1 is true or will run and 0 to false
 clean_system=1 #1 # Need cleanup_system_JBs.sh - https://github.com/ryuuzaki42/02_Scripts_Linux/ installed in /usr/bin/
+user_clean="j"
 log_file="/var/log/cleanup_system.log"
 
 if [ "$clean_system" == 1 ]; then
-    /usr/bin/cleanup_system_JBs.sh j y n # As user j - normal clean
     echo -e "\n # Running cleanup_system_JBs.sh - save log in: $log_file" #"
+    date > "$log_file"
 
+    /usr/bin/cleanup_system_JBs.sh "$user_clean" y n >> "$log_file" # As $user_clean - normal clean
 
-    #/usr/bin/cleanup_system_JBs.sh root y all y # Clean all as root - Be careful
     /usr/bin/cleanup_system_JBs.sh root y n >> "$log_file" # As root - normal clean
+
+    #/usr/bin/cleanup_system_JBs.sh root y all y >> "$log_file" # As root - Full clean, Be careful!
 fi
 
 # - JB ce -
