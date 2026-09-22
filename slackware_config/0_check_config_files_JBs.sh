b15d622f048d027b9e9af359575acab23c5a9b71
João Batista <joao42lbatista@gmail.com>
2026-09-22 10:36:56 -0300

    0_check_config_files_JBs.sh - update license

--- slackware_config/0_check_config_files_JBs.sh
+++ slackware_config/0_check_config_files_JBs.sh
@@ -5,24 +5,19 @@
 # me envie um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Este programa é um software livre; você pode redistribui-lo e/ou
-# modifica-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença, ou (na sua opinião) qualquer versão.
+# Este programa é um software livre: você pode redistribuí-lo e/ou
+# modificá-lo sob os termos da Licença Pública Geral GNU (GPL)
+# conforme publicada pela Free Software Foundation, tanto a versão 3
+# da licença, como (a seu critério) qualquer versão posterior.
 #
-# Este programa é distribuído na esperança que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
-# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
-#
-# Veja a Licença Pública Geral GNU para mais detalhes.
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, escreva para a Fundação do Software
-#
-# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+# Este programa é distribuído na esperança de que seja útil,
+# mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO. Consulte a
+# Licença Pública Geral do GNU para mais detalhes.
 #
 # Script: Check Slackware configurations files are equal with the system
 #
-# Last update: 08/07/2026
+# Last update: 22/09/2026
 #
 echo -e "\nUse with program to compare the files?"
 echo -n "1 diff, 2 sdiff, 3 kdiff3, 4 smartsynchronize, 5 other (default is 4): "
