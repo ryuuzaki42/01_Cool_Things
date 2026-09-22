e6d4e7d3d9e8ff3264ba0b1d009e3b6359eab54d
João Batista <joao42lbatista@gmail.com>
2026-09-22 10:38:43 -0300

    fstrim_JBs.sh- update license

--- slackware_config/fstrim_JBs.sh
+++ slackware_config/fstrim_JBs.sh
@@ -5,20 +5,15 @@
 # me envie um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Este programa é um software livre; você pode redistribui-lo e/ou
-# modifica-lo dentro dos termos da Licença Pública Geral GNU como
-# publicada pela Fundação do Software Livre (FSF); na versão 2 da
-# Licença, ou (na sua opinião) qualquer versão.
+# Este programa é um software livre: você pode redistribuí-lo e/ou
+# modificá-lo sob os termos da Licença Pública Geral GNU (GPL)
+# conforme publicada pela Free Software Foundation, tanto a versão 3
+# da licença, como (a seu critério) qualquer versão posterior.
 #
-# Este programa é distribuído na esperança que possa ser útil,
-# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
-# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
-#
-# Veja a Licença Pública Geral GNU para mais detalhes.
-# Você deve ter recebido uma cópia da Licença Pública Geral GNU
-# junto com este programa, se não, escreva para a Fundação do Software
-#
-# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+# Este programa é distribuído na esperança de que seja útil,
+# mas SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UM PROPÓSITO ESPECÍFICO. Consulte a
+# Licença Pública Geral do GNU para mais detalhes.
 #
 # Script: fstrim yours partitions in a SSD
 #
